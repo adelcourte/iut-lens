9acebe690cbe84df87211fdc7e05c9f032c31733
Antoine Delcourte <antoine.delcourte@gmail.com>
2021-06-03 10:30:15 +0200

    Article : updating page

--- wp-content/themes/iut-lens/single.php
+++ wp-content/themes/iut-lens/single.php
@@ -2,20 +2,23 @@
 
 <?php while(have_posts()) : ?>
     <?php the_post(); ?>
-    <section class="mgv-5">
+    <section class="mgt-7_5 mgb-5">
         <div class="row align-center-center">
-            <div class="column-12 mgb-5">
-                <div class="size-100 pgb-50-percent content-empty" style="background: url('<?=get_the_post_thumbnail_url(); ?>') no-repeat center/cover;"></div>
+            <div class="column-8 align-end-start mgb-2_5">
+                <span class="mgb-0_5"><?php the_date(); ?></span>
+                <h1 class="title--huge size-fit"><?php the_title(); ?></h1>
             </div>
-            <div class="column-8">
-                <h1 class="title--extra"><?php the_title(); ?></h1>
+            <div class="column-10">
+                <div class="size-100 pgb-50-percent content-empty" style="background: url('<?=get_the_post_thumbnail_url(); ?>') no-repeat center/cover;"></div>
             </div>
         </div>
     </section>
     <section>
         <div class="row align-center-center">
-            <div class="column-8 align-center-center post-content">
-                <?php the_content(); ?>
+            <div class="column-8 align-center-center">
+                <div class="content flex layout-column align-center-center">
+                    <?php the_content(); ?>
+                </div>
             </div>
         </div>
     </section>
